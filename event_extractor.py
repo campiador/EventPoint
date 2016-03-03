cf9a4f28ffb9b4f705271907699c893e05a98ec5
campiador <behnam.heydarshahi@gmail.com>
2016-03-03 12:31:29 -0500

    comm, sync

--- event_extractor.py
+++ event_extractor.py
@@ -1,4 +1,7 @@
 #Event Extractor by Behnam Heydarshahi
+#For tracing, we need to traverese through multiple threads, for that purpose, we have producer events in input
+#which means we have to back-traverse.
+
 from event import Event;
 for i in range(1, 2):
     f = open('thread1', 'r')
@@ -11,3 +14,12 @@ for i in range(1, 2):
         print (line)   #for line in f:
     #    print line
     f.close()
+
+#NEEDSWORK
+define findBarrier(thread1, thread2):
+    for event1 in thread1
+        if(event1.instanceOf(communication) && event1.getPthreadCallType == 5)
+            for event2 in thread2
+                if(event1.instanceOf(communication) && getPThreadCallType == 5)
+                    if (event2.getSyncStruct() == event2.getSyncStruct())
+                        #Barrierfound between th1 and th2
